907066d1b62e30e193d4d4f587c307b8c1e840f3
z1ndabad <taimur.samee@gmail.com>
2024-08-29 14:46:53 -0400

    animated character

---
+++ README.md
@@ -0,0 +1,7 @@
+# three.js tests
+Experiments with 3D rendering in the browser.
+
+See:
+- (https://threejs.org/)
+- (https://r3f.docs.pmnd.rs/)
+- (https://discoverthreejs.com/)
--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,22 @@ import viteLogo from "/vite.svg";
 import { Card } from "@/components/ui/card";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
+import { Player } from "@/components/mesh/Player";
 
 function App() {
   return (
-    <div id="app" className="flex flex-col items-center text-center">
+    <div id="app" className="flex h-full flex-col items-center text-center">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Card id="canvas-container" className="bg-foreground">
-        <Canvas camera={{ position: [0, 0, 0] }}>
-          <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial />
-          </mesh>
+      <Card id="canvas-container" className="bg-foreground h-3/4 w-3/4">
+        <Canvas camera={{ position: [0, 0, 10] }}>
+          <ambientLight intensity={0.01} />
+          <directionalLight color="white" position={[5, 0, 0]} />
+          <Player />
         </Canvas>
       </Card>
       <p className="read-the-docs">
