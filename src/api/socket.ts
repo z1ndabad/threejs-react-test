fc2817e79ebdf68f5246f7d1bee2cae352e00bfc
z1ndabad <taimur.samee@gmail.com>
2024-09-02 17:03:36 -0400

    fixed incorrect 3d marker numbers displaying by removing altitude prop in HollowGlobe -- figure out how to calculate altitude later

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -5,7 +5,10 @@ import { OpenSkyResponse } from "./opensky";
 function useAircraftPositions() {
   // TODO: Remember to replace prod API endpoint with wss:// for SSL
   const socketUrl = "ws://localhost:3000";
-  const [aircraftData, setAircraftData] = useState<OpenSkyResponse>();
+  const [aircraftData, setAircraftData] = useState<OpenSkyResponse>({
+    time: -1,
+    states: [],
+  });
   const { lastMessage } = useWebSocket(socketUrl);
 
   useEffect(() => {
--- src/components/mesh/HollowGlobe.tsx
+++ src/components/mesh/HollowGlobe.tsx
@@ -1,7 +1,8 @@
 import { FeatureCollection, GeoJsonProperties } from "geojson";
-import { useMemo } from "react";
-import Globe, { GlobeProps } from "react-globe.gl";
+import { useEffect, useMemo, useRef } from "react";
+import Globe, { GlobeMethods, GlobeProps } from "react-globe.gl";
 import {
+  CircleGeometry,
   DoubleSide,
   Mesh,
   MeshLambertMaterial,
@@ -10,9 +11,19 @@ import {
 import * as topoJson from "topojson-client";
 import { Topology, Objects } from "topojson-specification";
 import * as landJson from "world-atlas/land-110m.json";
-import { OpenSkyResponse } from "@/api/opensky";
+import { OpenSkyResponse, StateVector } from "@/api/opensky";
 import { useAircraftPositions } from "@/api/socket";
 
+type GlobePoint = {
+  label: string;
+  longitude: number;
+  latitude: number;
+  altitude: number;
+};
+
+// TODO - bug: incorrect number of 3D object markers render on the globe. Verified that
+// the correct # of datapoints is passed to the Scene, and that it works properly using
+// HTML element markers instead of 3D objects
 function HollowGlobe(props: GlobeProps) {
   const landFeatures = useMemo(() => {
     return topoJson.feature(
@@ -28,44 +39,58 @@ function HollowGlobe(props: GlobeProps) {
     side: DoubleSide,
   });
 
-  const planeMarker = useMemo(() => {
-    // TODO: scale these based on initial globe size (in case container grows/shrinks)
-    const planeGeometry = new OctahedronGeometry(1);
-    const planeMaterial = new MeshLambertMaterial({ color: "red" });
-    return new Mesh(planeGeometry, planeMaterial);
-  }, []);
-
   const { aircraftData } = useAircraftPositions();
   // TODO: add persisent labels layer?
   // TODO: positions are not reliable -- when a plane leaves the array, we should assume it keeps going the same directions
   // also, why are they not really moving? Do I need a longer monitoring window?
-  const positions = aircraftData?.states.map(
+  const positions: GlobePoint[] = aircraftData.states.map(
     ({ icao24, latitude, longitude, baro_altitude }) => {
       return {
-        label: icao24,
-        lat: latitude,
-        lng: longitude,
-        alt: baro_altitude,
+        label: icao24 ?? "",
+        latitude: latitude ?? 0,
+        longitude: longitude ?? 0,
+        altitude: baro_altitude ?? 0,
       };
     },
   );
   console.log(aircraftData);
+  const globeRef = useRef<GlobeMethods>();
+
+  const aircraftMarker = useMemo(() => {
+    // TODO: scale these based on initial globe size (in case container grows/shrinks)
+    const aircraftGeometry = new OctahedronGeometry(0.25);
+    const aircraftMaterial = new MeshLambertMaterial({ color: "red" });
+    return new Mesh(aircraftGeometry, aircraftMaterial);
+  }, [aircraftData]);
+
+  useEffect(() => {
+    if (globeRef.current) {
+      const scene = globeRef.current.scene;
+      console.log("SCENE");
+      console.log(scene().toJSON());
+    }
+  }, [positions]);
 
   return (
     <Globe
-      /* ref={globeRef} */
+      ref={globeRef}
       backgroundColor="black" // TODO: theme this later
       showGlobe={false}
       showAtmosphere={false}
       polygonsData={landPolygons}
       polygonCapMaterial={polygonsMaterial}
       polygonSideColor={() => "rgba(0, 0, 0, 0)"}
+      polygonAltitude={0} // defaults to 0.01 -- marker altitude needs to be adjusted when using a nonzero value
       objectsData={positions}
-      objectLabel="label"
-      objectLat="lat"
-      objectLng="lng"
-      objectAltitude="alt"
-      objectThreeObject={planeMarker}
+      // TODO: theming for accessors
+      objectLabel={(d) =>
+        `<div style='color: red'>${(d as GlobePoint).label}</div>`
+      }
+      objectLat={"latitude"}
+      objectLng={"longitude"}
+      // TODO: figure out correct altitude calculations--passing it directly draws markers in the wrong positons
+      objectAltitude={0}
+      objectThreeObject={aircraftMarker}
       {...props}
     />
   );
