ac5f6e324b34d71fddc6578dcd1aff7dcfeeb828
z1ndabad <taimur.samee@gmail.com>
2024-09-02 23:15:30 -0400

    Updated opensky API response type to reflect nullable values

--- src/api/opensky.d.ts
+++ src/api/opensky.d.ts
@@ -5,20 +5,20 @@ export type OpenSkyResponse = {
 
 export type StateVector = {
   icao24: string;
-  callsign: string;
+  callsign: string | null;
   origin_country: string;
-  time_position: number;
+  time_position: number | null;
   last_contact: number;
-  longitude: number;
-  latitude: number;
-  baro_altitude: number;
+  longitude: number | null;
+  latitude: number | null;
+  baro_altitude: number | null;
   on_ground: boolean;
-  velocity: number;
-  true_track: number;
-  vertical_rate: number;
+  velocity: number | null;
+  true_track: number | null;
+  vertical_rate: number | null;
   sensors: number[];
-  geo_altitude: number;
-  squawk: string;
+  geo_altitude: number | null;
+  squawk: string | null;
   spi: boolean;
   position_source: number;
   category: number;
